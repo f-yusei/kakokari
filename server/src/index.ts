b81d656f1e3ce6b50087568333e44ad06a07580e
f-yusei <fkys2932@gmail.com>
2023-07-05 06:44:32 +0900

    chore

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,12 @@
-"use strict"
-//こんにちはと出力
-console.log("こんにちは");
+import express from 'express';
+const app: express.Express = express();
+
+const PORT:number = 3300
+
+app.get('/', (req: express.Request, res: express.Response) => {
+    res.send('Hello World!')
+})
+
+app.listen(PORT, () => {
+    console.log("ローカルサーバー起動中")
+})
