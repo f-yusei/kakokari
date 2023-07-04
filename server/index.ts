e6f19fcfad884ea8e382c5f7e31e9a7ee001e255
f-yusei <fkys2932@gmail.com>
2023-07-05 08:46:22 +0900

    chore

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,15 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import CryptoJS from "crypto-js";
+import User from "./src/v1/models/user";
 const app: express.Express = express();
 const PORT: number = 3300
 dotenv.config()
+
+app.listen(PORT, () => {
+    console.log("ローカルサーバー起動中")
+})
 
 try {
     if (process.env.MONGO_URL) {
@@ -17,6 +23,18 @@ try {
 }
 
 
-app.listen(PORT, () => {
-    console.log("ローカルサーバー起動中")
-})
+app.post("/register", (req, res) => {
+    const password = req.body.password;
+
+    try {
+        // password hash by AES
+        req.body.password = CryptoJS.AES.encrypt(
+            password,
+            process.env.SECRET_KEY || ""
+        ).toString();
+        const user = await Use
+
+    } catch (err) {
+        console.log(err)
+    }
+});
