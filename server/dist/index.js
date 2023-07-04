e95dcc38d71ed5538b01c3ea42e17676d45248df
f-yusei <fkys2932@gmail.com>
2023-07-05 07:34:27 +0900

    feat:データベースと接続した

--- server/dist/index.js
+++ server/dist/index.js
@@ -4,11 +4,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const mongoose_1 = __importDefault(require("mongoose"));
 const app = (0, express_1.default)();
 const PORT = 3300;
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+const uri = "mongodb+srv://fkys2932:C4y0iGGwGTdwZtNO@cluster0.ysqni7b.mongodb.net/?retryWrites=true&w=majority";
+try {
+    mongoose_1.default.connect(uri);
+}
+catch (err) {
+    console.log(err);
+}
 app.listen(PORT, () => {
     console.log("ローカルサーバー起動中");
 });
--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,21 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 const app: express.Express = express();
-
-
-
 const PORT: number = 3300
+dotenv.config()
 
-app.get('/', (req: express.Request, res: express.Response) => {
-    res.send('Hello World!')
-})
+try {
+    if (process.env.MONGO_URL) {
+        mongoose.connect(process.env.MONGO_URL);
+        console.log("DB接続中")
+    } else {
+        console.error("MONGO_URL is not defined in the environment variables.");
+    }
+} catch (err) {
+    console.log(err)
+}
+
 
 app.listen(PORT, () => {
     console.log("ローカルサーバー起動中")
