c452b0f35bb6a7d1eb387be90b1792c8eec69967
f-yusei <fkys2932@gmail.com>
2023-07-05 16:53:25 +0900

    chore: install express-validator

--- server/index.ts
+++ server/index.ts
@@ -49,3 +49,28 @@ app.post("/register", async (req, res) => {
         return res.status(500).json(err);
     }
 });
+
+
+app.get("/login", async (req, res) => {
+    const password = req.body.password;
+
+    try {
+        // password hash by AES
+        req.body.password = CryptoJS.AES.encrypt(
+            password,
+            process.env.SECRET_KEY || ""
+        ).toString();
+        const user = await User.create(req.body);
+        //JWTの発行
+        const accessToken = JWT.sign({
+            id: user._id,
+        },
+            process.env.JWT_SECRET_KEY, {
+            expiresIn: "24h",
+        }
+        )
+        return res.status(200).json({ user, accessToken });
+    } catch (err) {
+        return res.status(500).json(err);
+    }
+});
