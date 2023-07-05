f25fc4ac91ac1a4039a13c837cac1b80f75d2dc4
f-yusei <fkys2932@gmail.com>
2023-07-05 10:50:53 +0900

    feat:ユーザー登録APIを作成した

--- server/index.ts
+++ server/index.ts
@@ -2,10 +2,13 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import CryptoJS from "crypto-js";
-import User from "./src/v1/models/user";
+const JWT = require("jsonwebtoken");
+const User = require("./src/v1/models/user.ts");
 const app: express.Express = express();
 const PORT: number = 3300
 dotenv.config()
+
+app.use(express.json());
 
 app.listen(PORT, () => {
     console.log("ローカルサーバー起動中")
@@ -23,7 +26,7 @@ try {
 }
 
 
-app.post("/register", (req, res) => {
+app.post("/register", async (req, res) => {
     const password = req.body.password;
 
     try {
@@ -32,9 +35,17 @@ app.post("/register", (req, res) => {
             password,
             process.env.SECRET_KEY || ""
         ).toString();
-        const user = await Use
-
+        const user = await User.create(req.body);
+        //JWTの発行
+        const accessToken = JWT.sign({
+            id: user._id,
+        },
+            process.env.JWT_SECRET_KEY, {
+            expiresIn: "24h",
+        }
+        )
+        return res.status(200).json({ user, accessToken });
     } catch (err) {
-        console.log(err)
+        return res.status(500).json(err);
     }
 });
