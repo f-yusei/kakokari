1f3891c32f9d4839481427ff9e844735adca2ecb
f-yusei <fkys2932@gmail.com>
2023-07-05 16:50:52 +0900

    feat:全体のテーマを設定

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,18 +3,31 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AuthLayout from './components/layout/AuthLayout';
 import Login from './features/auth/routes/Login';
 import Register from './features/auth/routes/Register';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+import { blue, red } from '@mui/material/colors';
 
 
 function App() {
+
+  const theme = createTheme({
+    palette: {
+      primary: blue
+    }
+  })
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<AuthLayout />}>
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-        </Route>
-      </Routes>
-    </BrowserRouter >
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AuthLayout />}>
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+          </Route>
+        </Routes>
+      </BrowserRouter >
+    </ThemeProvider>
   );
 }
 
