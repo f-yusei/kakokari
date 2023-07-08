19478bc319c7b1ba77c872ed778879195f521047
f-yusei <fkys2932@gmail.com>
2023-07-08 21:37:12 +0900

    feat: トークンがないときにログインに遷移する機能を実装

--- client/src/components/layout/MainLayout.tsx
+++ client/src/components/layout/MainLayout.tsx
@@ -1,12 +1,26 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Box, Container } from '@mui/material'
+import React, { useEffect } from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
+import authUtils from '../../utils/authUtils'
 
 const MainLayout = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        //JWTトークン認証
+        const checkAuth = async () => {
+            //認証チェック
+            const email = await authUtils.isAuthenticated();
+            // if (!email) {
+            //     navigate("/login");
+            // }
+        }
+        checkAuth()
+    }, [navigate])
     return (
-        <>
-            <div>main</div>
+        <Box>
             <Outlet />
-        </>
+        </Box>
     )
 }
 
