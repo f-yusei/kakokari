6b4c340a88abc6e99d4b6527c0dc594ded21e0b1
f-yusei <fkys2932@gmail.com>
2023-07-09 23:02:38 +0900

    update: ログイン状態をreduxで管理

--- client/src/components/layout/MainLayout.tsx
+++ client/src/components/layout/MainLayout.tsx
@@ -2,18 +2,23 @@ import { Box, Container } from '@mui/material'
 import React, { useEffect } from 'react'
 import { Outlet, useNavigate } from 'react-router-dom'
 import authUtils from '../../utils/authUtils'
+import { useDispatch } from 'react-redux'
+import { setUser } from '../../redux/features/userSlice'
 
 const MainLayout = () => {
     const navigate = useNavigate();
+    const dispatch = useDispatch();
 
     useEffect(() => {
         //JWTトークン認証
         const checkAuth = async () => {
             //認証チェック
             const email = await authUtils.isAuthenticated();
-            // if (!email) {
-            //     navigate("/login");
-            // }
+            if (!email) {
+                //navigate("/login");
+            } else {
+                dispatch(setUser(email))
+            }
         }
         checkAuth()
     }, [navigate])
