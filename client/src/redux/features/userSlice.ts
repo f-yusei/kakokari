314b01223fd9b87d6243f9c6241b157e6fdc67d7
f-yusei <fkys2932@gmail.com>
2023-07-09 09:08:15 +0900

    feat: user管理用のsliceを作成

---
+++ client/src/redux/features/userSlice.ts
@@ -0,0 +1,24 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface UserState {
+    value: {
+        email?: string;
+        username?: string;
+        token?: string;
+    };
+}
+
+const initialState: UserState = { value: {} };
+
+export const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+        setUser: (state, action: PayloadAction<object>) => {
+            state.value = action.payload;
+        }
+    }
+});
+
+export const { setUser } = userSlice.actions;
+export default userSlice.reducer;
