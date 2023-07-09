d1d646b24bce84df81367e44c9481cb63fa80dce
f-yusei <fkys2932@gmail.com>
2023-07-09 21:55:15 +0900

    style

--- server/src/v1/controllers/user.ts
+++ server/src/v1/controllers/user.ts
@@ -15,14 +15,14 @@ exports.register = async (req: express.Request, res: express.Response) => {
         ).toString();
         const user = await User.create(req.body);
         //JWTの発行
-        const token = JWT.sign({
+        const accessToken = JWT.sign({
             id: user._id,
         },
             process.env.JWT_SECRET_KEY, {
             expiresIn: "24h",
         }
         )
-        return res.status(200).json({ user, token });
+        return res.status(200).json({ user, accessToken });
     } catch (err) {
         return res.status(500).json(err);
     }
@@ -70,45 +70,3 @@ exports.login = async (req: express.Request, res: express.Response) => {
     }
 }
 
-
-exports.loginUsername = async (req: express.Request, res: express.Response) => {
-    const { username, password } = req.body;
-
-    try {
-        // メールアドレスの一致するユーザーを検索
-        const user = await User.findOne({ username: username });
-        if (!user) {
-            res.status(401).json({
-                error: [{
-                    param: "username",
-                    message: "ユーザー名が無効です",
-                }]
-            })
-        }
-        // パスワードの復号化
-        const decryptedPassword = CryptoJS.AES.decrypt(
-            user.password,
-            process.env.SECRET_KEY || ""
-        ).toString(CryptoJS.enc.Utf8);
-
-        if (decryptedPassword !== password) {
-            return res.status(401).json({
-                error: [{
-                    param: "password",
-                    message: "パスワードが無効です",
-                }]
-            })
-        }
-        //JWTの発行
-        const accessToken = JWT.sign({
-            id: user._id,
-        },
-            process.env.JWT_SECRET_KEY, {
-            expiresIn: "24h",
-        });
-
-        return res.status(201).json({ user, accessToken });
-    } catch (err) {
-        return res.status(500).json(err);
-    }
-}
