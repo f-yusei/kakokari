3db6f9137b9b228d8245e0ec50beac85a57984a3
f-yusei <fkys2932@gmail.com>
2023-07-10 18:06:18 +0900

    feat: 過去問のスキーマを作成

---
+++ server/src/v1/models/pastExam.ts
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const pastExamSchema = new mongoose.Schema({
+    seller: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    title: {
+        type: String,
+        default: "無題",
+        required: true,
+    },
+    grade: {
+        type: Number,
+        required: true,
+    },
+    department: {
+        type: String,
+        required: true,
+    },
+    subject: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        default: "ここに自由に記入してください",
+    },
+    year: {
+        type: Number,
+        required: true,
+    },
+    //どの学期の試験か
+    semester: {
+        type: String,
+        required: true,
+    }
+}, {
+    timestamps: true,  // createdAtとupdatedAtを自動的に管理
+});
+
+module.exports = mongoose.model("PastExam", pastExamSchema);
